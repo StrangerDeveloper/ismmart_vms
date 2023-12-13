ed48e30a9fe03267a1a3fe74d56cc82fbc65992f
Hasnain-Mirrani07 <115531664+Hasnain-Mirrani07@users.noreply.github.com>
2023-12-13 13:59:48 +0500

    setting raw

---
+++ lib/screens/setting/settings_view.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ismmart_vms/helper/languages/translations_key.dart' as langKey;
+import 'package:ismmart_vms/screens/setting/settings_viewmodel.dart';
+import '../../helper/constants.dart';
+import '../../helper/routes.dart';
+import '../../widgets/custom_text.dart';
+
+class SettingsView extends StatelessWidget {
+  SettingsView({Key? key}) : super(key: key);
+  final SettingViewModel viewModel = Get.put(SettingViewModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(0),
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ), //BoxDecoration
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.green),
+              accountName: Text(
+                "Hasnain Mirrani",
+                style: TextStyle(fontSize: 18),
+              ),
+              accountEmail: Text("hmirrani@gmail.com"),
+              currentAccountPictureSize: Size.square(50),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Color.fromARGB(255, 165, 255, 137),
+                child: Text(
+                  "A",
+                  style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                ), //Text
+              ), //circleAvatar
+            ), //UserAccountDrawerHeader
+          ), //DrawerHeader
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text(' My Profile '),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.book),
+            title: const Text('Store '),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.workspace_premium),
+            title: const Text(' Banking '),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
