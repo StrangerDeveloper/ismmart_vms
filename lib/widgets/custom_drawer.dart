1cdfc9ce2cad5070b4120df976fa5c454505612f
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-12-22 13:05:56 +0500

    Update custom_drawer.dart

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -5,7 +5,6 @@ import 'package:ismmart_vms/helper/constants.dart';
 import 'package:ismmart_vms/screens/collection/collection_view.dart';
 import 'package:ismmart_vms/screens/product_list/product_list_view.dart';
 
-import '../screens/Order/order_container/order_container_view.dart';
 import '../screens/Order/order_view.dart';
 import '../screens/auth/login/login_view.dart';
 import '../screens/setting/settings_view.dart';
@@ -54,7 +53,7 @@ class CustomDrawer extends StatelessWidget {
                     icon: Icons.add_shopping_cart,
                     onTap: () {
                       Get.back();
-                      Get.to(() => OrderContainerView());
+                      Get.to(() => OrderView());
                     },
                   ),
                   drawerItem(
