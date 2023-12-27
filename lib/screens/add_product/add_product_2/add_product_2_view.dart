c93d6f11eb3567209a14e604a4750738404cecea
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-12-27 16:06:08 +0500

    Update add_product_2_view.dart

--- lib/screens/add_product/add_product_2/add_product_2_view.dart
+++ lib/screens/add_product/add_product_2/add_product_2_view.dart
@@ -73,56 +73,6 @@ class AddProduct2View extends StatelessWidget {
     );
   }
 
-  // Widget stepperText() {
-  //   return Padding(
-  //     padding: const EdgeInsets.only(top: 20, bottom: 25),
-  //     child: RichText(
-  //       overflow: TextOverflow.ellipsis,
-  //       text: const TextSpan(
-  //         style: TextStyle(
-  //           color: ThemeHelper.blue1,
-  //           fontWeight: FontWeight.w500,
-  //         ),
-  //         children: [
-  //           TextSpan(
-  //             text: "  /  ",
-  //             style: TextStyle(
-  //               color: ThemeHelper.grey6,
-  //             ),
-  //           ),
-  //           TextSpan(
-  //             text: "Product & Pricing ",
-  //             style: TextStyle(
-  //               color: ThemeHelper.grey2,
-  //             ),
-  //           ),
-  //           TextSpan(
-  //             text: "  /  ",
-  //             style: TextStyle(
-  //               color: ThemeHelper.grey6,
-  //             ),
-  //           ),
-  //           TextSpan(
-  //             text: "Variant Setting",
-  //             style: TextStyle(
-  //               color: ThemeHelper.grey2,
-  //             ),
-  //           ),
-  //           TextSpan(
-  //             text: "  /  ",
-  //             style: TextStyle(
-  //               color: ThemeHelper.grey6,
-  //             ),
-  //           ),
-  //           TextSpan(
-  //             text: "Shipping",
-  //           ),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
-
   Widget variantsContainer(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
