c1cedef0157fd1c4f648f1bb7e68e2de1dd8d4f6
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-12-28 17:17:02 +0500

    Update urls.dart

--- lib/helper/urls.dart
+++ lib/helper/urls.dart
@@ -8,4 +8,6 @@ class Urls {
   // static String baseUrl = "https:///ismmart-ecommerce-backend-e233368b3b0d.herokuapp.com/api";
   static String getPayout = "/vendor/payout?limit=10&page=";
   static String getLocation = "/vendor/location?limit=10&page=";
+
+  static String getOrder = "/vendor/order?limit=10&page=";
 }
