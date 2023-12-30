30866b91a4a663464b7d118d41ea14d52ce29ff8
hayatbangash <hayatbangash66@gmail.com>
2023-12-30 13:35:40 +0500

    addBank & bankList UI -- completed.

--- lib/screens/add_bank/add_bank_view.dart
+++ lib/screens/add_bank/add_bank_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_multi_formatter/formatters/masked_input_formatter.dart';
+import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
