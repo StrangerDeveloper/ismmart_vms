517d59986eea5f854f8f6adf4b0bb01251b74a88
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-01-02 15:40:17 +0500

    Update cancel_order_viewModel.dart

--- lib/screens/order_detail/cancel_order_viewModel.dart
+++ lib/screens/order_detail/cancel_order_viewModel.dart
@@ -1,19 +1,69 @@
 import 'package:get/get.dart';
+import 'package:ismmart_vms/helper/languages/translations_key.dart';
+import 'package:ismmart_vms/screens/order_detail/order_detail_viewModel.dart';
 
+import '../../helper/api_base_helper.dart';
+import '../../helper/urls.dart';
 import '../order_listing/model/orderModel.dart';
+import '../order_listing/order_viewModel.dart';
 
 class CancelORderViewMOdel extends GetxController {
+  OrderListingViewModel orderListingViewModel = Get.find();
+  OrderDetailViewModel orderDetailViewModel = Get.find();
+
   RxBool selectAllValue = false.obs;
   Rx<OrderItem> orderItemModel = OrderItem().obs;
   RxList<Lineitem> lineItemList = <Lineitem>[].obs;
   RxList<Lineitem> selectedItems = <Lineitem>[].obs;
 
-
   @override
   void onInit() {
     orderItemModel.value = Get.arguments['model'];
     lineItemList.addAll(orderItemModel.value.lineitems ?? []);
+    lineItemList.forEach((element) {
+      print("Status ${element.sId}");
+    });
     super.onInit();
+  }
+
+  Future<void> updateOrder() async {
+    try {
+      //use put method for update
+      List<String> selectedIndicesId = [];
+      String selectedItemsStatus = "";
+
+      // Extract the indices of selected items
+      for (int i = 0; i < lineItemList.length; i++) {
+        if (lineItemList[i].isSelected == true) {
+          selectedIndicesId.add(lineItemList[i].sId!);
+          selectedItemsStatus = lineItemList[i].fulfilmentStatus!;
+        }
+      }
+      Map<String, dynamic> param = {
+        "itemsIndex": selectedIndicesId,
+        "status": selectedItemsStatus,
+      };
+      await ApiBaseHelper()
+          .putMethod(url: Urls.updateOrder, body: param)
+          .then((response) async {
+        if (response['success'] == true) {
+          print("Statusss ${response['success']}");
+          orderListingViewModel.lineItemList.clear();
+          orderListingViewModel.orderItemList.clear();
+          orderListingViewModel.lineItemList.refresh();
+          orderListingViewModel.orderItemList.refresh();
+          orderDetailViewModel.lineItemList.clear();
+          orderDetailViewModel.lineItemList.refresh();
+          orderDetailViewModel.selectedItems.clear();
+          orderDetailViewModel.selectedItems.refresh();
+
+          await orderListingViewModel.getOrderListing();
+          Get.back();
+        }
+      });
+    } catch (error) {
+      print(error);
+    }
   }
 
   selectAllItems() {
