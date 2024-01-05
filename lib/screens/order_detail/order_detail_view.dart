ce546922f552f433c2c4096d9a41d66d2415ccf3
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-01-05 11:55:28 +0500

    Update order_detail_view.dart

--- lib/screens/order_detail/order_detail_view.dart
+++ lib/screens/order_detail/order_detail_view.dart
@@ -516,7 +516,7 @@ class OrderDetailView extends StatelessWidget {
                                                   .value
                                                   .lineitems?[index]
                                                   .fulfilmentStatus ??
-                                              "status")
+                                              "status"),
                                         ],
                                       ),
                                       _customField2(
@@ -613,7 +613,7 @@ class OrderDetailView extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _status(viewModel.orderItemModel.value.fulfilmentStatus ??
+                  _status(viewModel.orderItemModel.value.deliveryStatus ??
                       "status"),
                   Padding(
                     padding: const EdgeInsets.only(top: 8, bottom: 5),
